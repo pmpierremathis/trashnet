c86298e444773e5db725c61f459a4db02083b56d
Aurelien Blond <aurelien.blond@gmail.com>
2021-07-07 17:35:53 +0200

    updated params with GCP project + updated fast.py to download picture

--- api/fast.py
+++ api/fast.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import UploadFile, File
 from trashnet.predict import read_image, load_model, CLASSES
 import numpy as np
+import shutil
+from datetime import datetime
+
 
 app = FastAPI()
 
@@ -22,6 +25,13 @@ model = load_model()
 
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
+    # Download locally the file
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    filename = f"{timestamp}.jpg"
+    with open(filename, "wb") as buffer:
+         shutil.copyfileobj(file.file, buffer)
+    
+    # Make a prediction
     image = read_image(await file.read())
     prediction_ohe = model.predict(image)
     prediction = np.argmax(prediction_ohe, axis=1)
