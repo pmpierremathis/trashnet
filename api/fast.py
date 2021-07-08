b01d0677de836200181968de490fad2b5c8f400f
Aurelien Blond <aurelien.blond@gmail.com>
2021-07-08 23:49:28 +0200

    Create a new endpoint to validate the label with the user

--- api/fast.py
+++ api/fast.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import UploadFile, File
+from fastapi import UploadFile, File, Form
 from trashnet.predict import read_image, load_model, CLASSES
 from trashnet.gcp import storage_upload
 import numpy as np
@@ -38,16 +38,22 @@ async def predict_api(file: UploadFile = File(...)):
     prediction = np.argmax(prediction_ohe, axis=1)
     api_answer = CLASSES[prediction[0]]
     
-     # Download locally the file
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    filename = f"{api_answer} {timestamp}.jpg"
+    return {"prediction" : api_answer,
+            "probability" : probability}
     
+@app.post("/labelling")
+async def upload_label(checked_label:str = Form(...), file: UploadFile = File(...)):
+    # Download locally the file
+    print(type(checked_label))
+    print(type(file))
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    filename = f"{checked_label} {timestamp}.jpg"
+    
+    image_bytes = await file.read()
     imageStream = io.BytesIO(image_bytes)
     imageFile = Image.open(imageStream)
     imageFile.save(filename)
 
     # # Upload the file to GCS      
-    storage_upload(filename, api_answer)
-    
-    return {"prediction" : api_answer,
-            "probability" : probability}
+    storage_upload(filename, checked_label)
+    pass
